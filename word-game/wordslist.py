5fac5a31f5a9fe4a1d1ed29f91699110cd059d86
JeevithaDurairaj <jeevithadurairaj2@gmail.com>
2026-09-02 18:29:28 +0530

    Add Python practice projects
    Adds three beginner Python projects:
    - number-guessing-game: guess a random number between 1-100
    - banking-program: simple menu-driven banking simulator
    - word-game: Hangman game with word list

---
+++ word-game/wordslist.py
@@ -0,0 +1,8 @@
+words =("dog", "cat", "elephant", "tiger", "lion", "horse", 
+        "rabbit", "monkey", "giraffe", "zebra", "panda", "bear",
+        "deer", "fox", "wolf", "cow", "goat", "sheep", "elephant", "parrot", "eagle", 
+        "pigeon", "sparrow", "peacock", "owl", "crow", 
+        "swan", "duck", "flamingo", "penguin", "woodpecker", 
+        "chair", "table", "laptop", "phone", "book",
+        "pencil", "bottle", "clock", "umbrella", "backpack",
+        "camera", "guitar", "lamp", "bicycle", "television", "keyboard", "mug", "scissors", "basket")
